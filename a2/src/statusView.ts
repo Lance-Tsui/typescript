e02aeffa730bf00e00e1453a6b6901bd1a40253f
Lance <s275xu@uwaterloo.ca>
2024-02-09 23:34:58 -0500

    a2 submission 7

--- a2/src/statusView.ts
+++ a2/src/statusView.ts
@@ -3,6 +3,7 @@ import {
     SKContainer,
     Layout,
     SKTextfield,
+    SKLabel,
   } from "simplekit/imperative-mode";
   
   // local imports
@@ -13,51 +14,28 @@ import {
     //#region observer pattern
   
     update(): void {
-      const id = this.model.selectId;
-      if (id !== null) {
-        this.button.text = "Update";
-        this.textfield.text = this.model.todo(id)?.text || "";
-      } else {
-        this.button.text = "Add";
-        this.textfield.text = "";
+        this.message.text = "hello world";
       }
-    }
-  
-    //#endregion
-  
-    button = new SKButton({ text: "?" });
-    textfield = new SKTextfield({ text: "?" });
-  
+    
+    message = new SKLabel({ text: "?" });
+
     constructor(private model: Model) {
       super();
   
       // setup the view
-      this.id = "entry";
+      this.id = "bottom";
       this.fill = "lightgrey";
       this.padding = 10;
+
   
       // try removing fillWidth and/or height
       this.fillWidth = 1;
       this.height = 50;
   
-      this.layoutMethod = Layout.makeFillRowLayout({ gap: 10 });
-  
-      // add widgets to the view
-      this.textfield.fillWidth = 1;
-      this.addChild(this.textfield);
-      this.addChild(this.button);
-  
-      // create controller
-      this.button.addEventListener("action", () => {
-        const text = this.textfield.text;
-        if (model.selectId !== null) {
-          model.update(model.selectId, { text });
-        } else {
-          model.create(text);
-        }
-        this.textfield.text = "";
-      });
-  
+      this.layoutMethod = Layout.makeCentredLayout();
+ 
+      this.addChild(this.message);
+
       // register with the model when we're ready
       this.model.addObserver(this);
     }
