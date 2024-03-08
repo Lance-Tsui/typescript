a5b7461358684882cb429f83c596869dc907b270
Lance <s275xu@uwaterloo.ca>
2024-03-07 23:56:20 -0500

    a3 submission 5

--- a3/src/todoView.ts
+++ a3/src/todoView.ts
@@ -11,16 +11,11 @@ export class TodoView implements View {
     const todo = this.model.todo(this.todoId);
     if (!todo) return;
     this.checkbox.checked = todo.done;
-    this.todoText.innerText = `${todo.text || "?"} (id#${todo.id})`;
-    this.selectButton.disabled = todo.done;
   }
 
   //#endregion
 
   checkbox = document.createElement("input");
-  todoText = document.createElement("span");
-  selectButton = document.createElement("button");
-  deleteButton = document.createElement("button");
 
   // the view root container
   private container: HTMLDivElement;
@@ -35,24 +30,13 @@ export class TodoView implements View {
 
     // setup the view
     this.checkbox.type = "checkbox";
-    this.deleteButton.innerText = "🗑️";
-    this.selectButton.innerText = "✏️";
 
     // add widgets to the view
     this.container.appendChild(this.checkbox);
-    this.container.appendChild(this.todoText);
-    this.container.appendChild(this.selectButton);
-    this.container.appendChild(this.deleteButton);
 
     // controllers
     this.checkbox.addEventListener("click", () => {
       model.update(todoId, { done: this.checkbox.checked });
-    });
-    this.deleteButton.addEventListener("click", () => {
-      model.delete(todoId);
-    });
-    this.selectButton.addEventListener("click", () => {
-      model.select(todoId);
     });
 
     // register with the model
