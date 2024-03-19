fbbab17224e369a42ddfeecc50f5ed8674971978
Lance <s275xu@uwaterloo.ca>
2024-03-19 15:51:22 -0400

    a4 submission 9

--- a4/src/Info.tsx
+++ a4/src/Info.tsx
@@ -1,20 +1,36 @@
+import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 import "./Info.css";
-
-// State and signal shortcuts:
-// (1) You can import state members directly without namespace.
-// (2) You can leave out the ".value" part of a signal when
-//     retrieving the value in JSX or HTM.
-// But you should only consider doing this when the state context
-// is clear, such as a small component (as is the case here).
-
-import { num, numDone, selectedTodoId } from "./state";
+import { todos, selectedTodoId, numDone, getTodo } from "./state";
 
 export default function Info() {
+  // 使用useState来存储选中的颜色值
+  const [selectedColor, setSelectedColor] = useState('');
+
+  useEffect(() => {
+    const todo = getTodo(selectedTodoId.value); // 使用getTodo函数获取当前选中的todo项
+    if (todo) {
+      setSelectedColor(todo.color); // 如果找到了todo，就更新selectedColor状态
+    } else {
+      setSelectedColor(''); // 否则，重置selectedColor
+    }
+  }, [selectedTodoId.value]); // 依赖于selectedTodoId的变化
+
   return (
     <div id="info">
-      {selectedTodoId.value != null
-        ? `edit id#${selectedTodoId}`
-        : `${num} todos (${numDone} done)`}
+      {numDone.value === 0 ? "Select One" : numDone.value === 1 ? (
+        <div className="editor">
+          <div className="display-area">
+            {/* Render the selected square here */}
+          </div>
+          <div className="form">
+            <label>
+              Hue:
+              <input type="number" value={selectedColor} min="0" max="360" />
+            </label>
+          </div>
+        </div>
+      ) : "Too Many Selected"}
     </div>
   );
 }
